7c43a39e0b15dd3e75916a58298fad2b1be1745c
Raghuds48 <raghavendrasak78@gmail.com>
2024-08-29 22:35:33 +0530

    Update jenkinsfile2.groovy

--- jenkins/jenkinsfile2.groovy
+++ jenkins/jenkinsfile2.groovy
@@ -1,1 +1,28 @@
+pipeline{
+   agent any
 
+  stages{
+    stage('build') {
+      steps{
+        sh '''
+        echo "Build is successful on build stage"
+        '''
+         }
+      }
+    stage('test') {
+      steps{
+        sh '''
+        echo "Test is successful on build stage"
+        '''
+         }
+      }
+    stage('Deploy') {
+      steps{
+        sh '''
+        echo "Deploy successful on Deploy stage"
+        '''
+         }
+      }
+  }  
+  
+}
