7dc809baee751e8e804e97cdf396606d0fbd4fd6
Raghuds48 <raghavendrasak78@gmail.com>
2024-08-29 23:02:17 +0530

    Update jenkinsfile2.groovy

--- jenkins/jenkinsfile2.groovy
+++ jenkins/jenkinsfile2.groovy
@@ -5,7 +5,8 @@ pipeline{
     stage('build') {
       steps{
         sh '''
-        echo "Build is successful on build stage"
+        echo "Build Docker image on `hostname`"
+        docker image build -t app_image:1.0 ${WORKSPACE}/docker
         '''
          }
       }
@@ -13,16 +14,25 @@ pipeline{
       steps{
         sh '''
         echo "Test is successful on build stage"
+        docker image ls | grep app_image
         '''
          }
       }
     stage('Deploy') {
       steps{
         sh '''
-        echo "Deploy successful on Deploy stage"
+        Docker container run -d --name c1 -p 9000:80 app_image:1.0
+        echo "Deploy Docker image on `hostname`"
         '''
          }
       }
-  }  
+     stage('verify'){
+                         steps{
+                                 sh '''
+                                        docker container ls | grep c1
+                                 '''
+                         }
+                 }
+          }  
   
 }
